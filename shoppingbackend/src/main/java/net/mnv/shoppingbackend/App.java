a302795a18cfd23fe4149c7f7879d6f8a927f720
Niteesh488 <niteeshvarma.mudunuri@gmail.com>
2017-12-23 19:50:12 +0530

    project setup completed successfully..

---
+++ shoppingbackend/src/main/java/net/mnv/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.mnv.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
